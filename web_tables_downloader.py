890f1518a29255f1965470b3013b254deae57806
fipaniagua <fipaniagua@uc.cl>
2019-02-06 01:14:01 -0300

    webscrapper and downloader working

---
+++ web_tables_downloader.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from time import sleep, time
+from bs4 import BeautifulSoup
+from openpyxl import Workbook, load_workbook
+
+def save_table(file_name, page_source, first = True):
+    if first:
+        new_wb = Workbook()
+    else:
+        new_wb = load_workbook(file_name)
+    new_wb.guess_types = False
+    new_wa = new_wb.active
+    soup = BeautifulSoup(page_source, 'html.parser')
+
+
+    ths = soup.find_all('th')
+    #ths = browser.find_elements_by_xpath('//*[@id="A2248:form-visualizar:datosplantilla_head"]//th'
+    header = [th.string for th in ths]
+    amount_colm = len(header)
+    if first:
+        new_wa.append(header)
+
+    #raw_data = browser.find_elements_by_xpath('//*[@id="A2248:form-visualizar:datosplantilla_data"]//td')
+    raw_data = soup.find_all('td')
+    lines = [cell.string.replace(".","") if cell.string != None else cell.string for cell in raw_data]
+    #lines = [cell.string for cell in raw_data]
+    for row_index in range(int(len(raw_data)/amount_colm)):
+        start = row_index*amount_colm
+        end = start+amount_colm
+        line = lines[start:end]
+        new_wa.append(line)
+
+    new_wb.save(file_name)
+    return
+
+def save_page(link, file_name):
+    browser.get(link)
+    first = True
+    while True:
+        sleep(1)
+        save_table(file_name, browser.page_source, first)
+        next = browser.find_elements_by_xpath('/html/body/div[1]/div/div[2]/div/div/div/div/div/div/div/div/div[4]/form/div/div[2]/div/div[2]/div/div[1]/span[4]')
+        if len(next) > 0 and not "disabled" in next[0].get_attribute("class"):
+            first = False
+            next_btn = browser.find_element_by_xpath('//*[@id="A2248:form-visualizar:datosplantilla_paginator_top"]/span[4]/span')
+            next_btn.click()
+        else:
+            break
+
+path_to_chromedriver = "./chromedriver"
+browser = webdriver.Chrome(executable_path = path_to_chromedriver)
+browser.implicitly_wait(4)
+
+'''
+t = time()
+save_page("https://www.portaltransparencia.cl/PortalPdT/pdtta/-/ta/AH001/PR/PHON/23448678", "test_1.xlsx")
+print("t:" , time() - t)
+'''
+
+
+wb =  load_workbook(filename='./datos_links.xlsx', read_only=True)
+ws = wb.active
+rows = list(ws.rows)[1:]
+i = 1
+succes = 0
+st = time()
+for row in rows[i-1:]:
+    cells = list(row)
+    file_name = "./data/{6}/{0}_{1}_{2}_{3}_{4}_({5}).xlsx".format(cells[0].value, cells[1].value, cells[4].value, cells[3].value, cells[2].value, i, selected_muni)
+    #file_name = "./data_muni/{6}/{0}_{1}_{2}_{3}_{4}_({5}).xlsx".format(cells[0].value, cells[1].value, cells[4].value, cells[3].value, cells[2].value, i, selected_muni)
+    print(file_name)
+    print("i:", i)
+    succes +=1
+    try:
+        save_page(cells[6].value, file_name)
+    except Exception as e:
+        succes -=1
+    i += 1
+    print("succes:", succes)
+    print("time:", time()-st)
--- webscrapper.py
+++ webscrapper.py
@@ -7,13 +7,21 @@ from selenium.webdriver.support import expected_conditions
 from time import sleep
 from openpyxl import Workbook, load_workbook
 
+def clean_extra_tabs():
+    handles = browser.window_handles
+    for i in range(len(handles)):
+            if i != 0:
+                browser.switch_to_window(handles[i])
+                browser.close()
+    browser.switch_to_window(handles[0])
 
-def save_meta_data(org, sub_org, tipo_contrato, mes, year, website):
-    wb = load_workbook('datos_links.xlsx')
+def save_meta_data(org, sub_org, tipo_contrato, mes, year, website, extra = None, second_colum = ""):
+    wb = load_workbook('./links_muni/datos_links_{0}.xlsx'.format(second_colum))
     ws = wb.active
-    ws.append([org, sub_org, tipo_contrato, mes, year, website])
-    wb.save('datos_links.xlsx')
+    ws.append([org, sub_org, tipo_contrato, mes, year, extra, website])
+    wb.save('./links_muni/datos_links_{0}.xlsx'.format(second_colum))
     return
+
 
 def get_options_by_class(class_name):
     browser.implicitly_wait(4)
@@ -24,10 +32,11 @@ def get_options_by_class(class_name):
     return sub_options
 
 
-def handler_second_window_1(org, sub_org, windows_3):
-    differents = [" Política de Remuneraciones del Consejo para la Transparencia ",
-                  " Histórico de Personal y Remuneraciones a Diciembre 2013 ",
-                  "Escala Remuneraciones (*)" ]
+
+def handler_second_window_1(org, sub_org, windows_3 = False, sub_option=""):
+    differents = ["Política de Remuneraciones del Consejo para la Transparencia",
+                  "Histórico de Personal y Remuneraciones a Diciembre 2013",
+                  "Escala Remuneraciones (*)", "Escala Remuneraciones" ]
     browser.switch_to_window(browser.window_handles[1])
     amount_contracts = len(browser.find_elements_by_xpath('//*[@id="A2248:form:j_idt30:0:datalist_list"]/li[4]//a'))
     for index_contract in range(amount_contracts):
@@ -57,16 +66,13 @@ def handler_second_window_1(org, sub_org, windows_3):
                             for index_link in range(amount_links):
                                 link = browser.find_elements_by_xpath('//*[@id="A2248:form-visualizar:preview-tipo-padre"]//a')[index_link]
                                 link_text = link.text
-                                link.click()
-                                save_meta_data(org, sub_org, tipo_contrato, mes_text, link_text, browser.current_url)
-                                browser.back()
-
+                                link_href = link.get_attribute('href')
+                                #link.click()
+                                save_meta_data(org, sub_org, tipo_contrato, mes_text, link_text, link_href, ano_text, sub_option)
                         else:
-                            save_meta_data(org, sub_org, tipo_contrato, ano_text, mes_text, browser.current_url)
-
+                            save_meta_data(org, sub_org, tipo_contrato, ano_text, mes_text, link_href, sub_option)
                         sleep(1)
                         browser.back()
-
                     browser.back()
 
             sleep(1)
@@ -87,10 +93,11 @@ browser.find_element_by_xpath(first_link_xpath).click()
 contents = browser.find_elements_by_class_name('dor_org_no_senialado')
 options = [content.get_attribute("id") for content in contents]
 
+start = False
 org_with_3options = [0,11,12]
 sub_org_with_3options = ["Corporaciones Municipales"]
-i = 0
-for id_option in options[:12]:
+i = 12
+for id_option in options[12:13]:
     print("scope1:", id_option, "i:", i)
     try:
         browser.get(page_url)
@@ -102,6 +109,8 @@ for id_option in options[:12]:
         browser.find_element_by_id(id_option).click()
 
     if i in org_with_3options :
+
+
         sub_options = get_options_by_class('dor_org_hijo_no_senialado')
         for sub_option in sub_options:
             print("scope2:", sub_option)
@@ -110,6 +119,7 @@ for id_option in options[:12]:
                 sub_org = browser.find_element_by_id(sub_option).text
                 browser.find_element_by_id(sub_option).click()
             except Exception as e2:
+                browser.implicitly_wait(5)
                 sub_org = browser.find_element_by_id(sub_option).text
                 browser.find_element_by_id(sub_option).click()
             sleep(1)
@@ -118,19 +128,30 @@ for id_option in options[:12]:
             for index_link in range(amount_links):
                 link = browser.find_elements_by_xpath('//*[@id="A3684:form:organismos"]//a')[index_link]
                 sub_org_name = link.text
-                print("***",sub_org_name)
+                print("*",sub_org_name)
+
+                if sub_org_name in "Municipalidad de Algarrobo":
+                    start = True
+                if not start:
+                    continue
+                if sub_org_name in "Municipalidad de Zapallar":
+                    raise Exception
+
+
                 link.click()
+                browser.implicitly_wait(15)
                 btn_to_new_window = browser.find_element_by_partial_link_text('Vea la informac')
+                browser.implicitly_wait(5)
                 if "portaltransparencia" in btn_to_new_window.get_attribute("href"):
-                    #btn_to_new_window.click()
                     if sub_org in sub_org_with_3options:
                         windows_3 = True
-                        btn_to_new_window.click()
-                        handler_second_window_1(org, sub_org_name, windows_3)
                     else:
                         windows_3 = False
-                    #handler_second_window_1(org, sub_org, windows_3)
-                sleep(2)
+                    windows_3 = True
+                    clean_extra_tabs()
+                    btn_to_new_window.click()
+                    handler_second_window_1(org, sub_org_name, windows_3, sub_org)
+                sleep(1)
                 try:
                     browser.get(page_url)
                     browser.find_element_by_id(id_option).click()
@@ -139,10 +160,14 @@ for id_option in options[:12]:
                     browser.get(page_url)
                     browser.find_element_by_id(id_option).click()
                     browser.find_element_by_id(sub_option).click()
+                sleep(1)
+
 
     elif i != 4:
+        browser.implicitly_wait(5)
         sub_contents = browser.find_elements_by_xpath("//*[@class='dor_organismos_selecc Class_id_link_org_link']")
         sub_options = [sub_content.get_attribute("id") for sub_content in sub_contents]
+        print(len(sub_options))
         for sub_option in sub_options:
             print(sub_option)
             sub_org = browser.find_element_by_id(sub_option).text
@@ -151,7 +176,7 @@ for id_option in options[:12]:
             if "portaltransparencia" in btn_to_new_window.get_attribute("href"):
                 btn_to_new_window.click()
                 handler_second_window_1(org, sub_org)
-            sleep(2)
+            sleep(1)
             try:
                 browser.get(page_url)
                 browser.find_element_by_id(id_option).click()
